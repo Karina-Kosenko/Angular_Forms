7a8e1a914eb77b65cdd4dc0f7dfb03813b199a86
Karina-Kosenko <karina.kosenko@techs.com.ua>
2020-08-05 12:31:33 +0300

    Section-15-point-196

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestionValue = 'pet';
   answer: '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
